7ef735a0244edf770459e9d500d38069c086bed7
salexey1990 <amsirotkin1990@gmail.com>
2016-09-27 22:26:51 +0300

    lesson 8

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var http = require('http');
 var path = require('path');
 var config = require('config');
 var log = require('libs/log')(module);
+var mongoose = require('libs/mongoose');
 var HttpError = require('error').HttpError;
 
 var app = express();
@@ -20,7 +21,23 @@ if (app.get('env') == 'development') {
 }
 
 app.use(express.bodyParser());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser());
+
+var MongoStore = require('connect-mongo')(express);
+
+
+app.use(express.session({
+	secret: config.get('session:secret'),
+	key: config.get('session:key'),
+	cookie: config.get('session:cookie'),
+	store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
+app.use(function ( req, res, next ) {
+	req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1;
+	res.send('Visits: ' + req.session.numberOfVisits);
+})
+
 app.use(require('middleware/sendHttpError'));
 app.use(app.router);
 require('routes')(app);
