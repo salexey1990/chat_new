b6d8bd4da28eedf4d50e30b0fa43362702a000ce
salexey1990 <amsirotkin1990@gmail.com>
2016-09-30 22:47:29 +0300

    lesson11

--- app.js
+++ app.js
@@ -63,7 +63,17 @@ app.use(function( err, req, res, next ) {
 	
 });
 
-http.createServer(app).listen(config.get('port'), function(){
+var server = http.createServer(app);
+server.listen(config.get('port'), function(){
   log.info('Express server listening on port ' + config.get('port'));
 });
 
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+	
+	socket.on('message', function(text, cb) {
+		socket.broadcast.emit('message', text);
+		cb('huy')
+	});
+});
